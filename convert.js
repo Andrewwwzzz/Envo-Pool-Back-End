3ab5bc24c7dc7341ddf64bf64ba86b8f82cd6d8b
zhong xian <zhongxian16@gmail.com>
2026-02-25 18:39:28 +0800

    Fix JWKS with proper RSA key export

--- convert.js
+++ convert.js
@@ -1,11 +1,15 @@
-// convert.js
-const { exportJWK } = require('jose');
-const fs = require('fs');
-const crypto = require('crypto');
+const { exportJWK } = require("jose");
+const fs = require("fs");
+const crypto = require("crypto");
+
+async function convert(file) {
+  const pem = fs.readFileSync(file);
+  const key = crypto.createPublicKey(pem);
+  const jwk = await exportJWK(key);
+  console.log(file, jwk);
+}
 
 (async () => {
-  const pub = fs.readFileSync('signing_public.pem');
-  const key = crypto.createPublicKey(pub);
-  const jwk = await exportJWK(key);
-  console.log(jwk);
+  await convert("signing_public.pem");
+  await convert("dpop_public.pem");
 })();
--- routes/jwks.routes.js
+++ routes/jwks.routes.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { exportJWK } = require("jose");
-const crypto = require("crypto");
 
-router.get("/.well-known/jwks.json", async (req, res) => {
-  const publicKeyPem = process.env.SIGNING_PUBLIC_KEY.replace(/\\n/g, "\n");
-
-  const keyObject = crypto.createPublicKey(publicKeyPem);
-  const jwk = await exportJWK(keyObject);
-
-  jwk.use = "sig";
-  jwk.alg = "ES256";
-  jwk.kid = process.env.SIGNING_KID;
-
-  res.json({ keys: [jwk] });
+router.get("/.well-known/jwks.json", (req, res) => {
+  res.json({
+    keys: [
+      {
+        kty: "EC",
+        crv: "P-256",
+        use: "sig",
+        alg: "ES256",
+        kid: process.env.SIGNING_KID,
+        x: process.env.SIGNING_PUBLIC_X,
+        y: process.env.SIGNING_PUBLIC_Y
+      }
+    ]
+  });
 });
 
 module.exports = router;
