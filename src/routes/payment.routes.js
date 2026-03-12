1ec93c781fba84e5b8a9b4bd3919dac5a7959e68
zhong xian <zhongxian16@gmail.com>
2026-03-12 18:46:27 +0800

    fix hardware_id mapping for booking

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -23,7 +23,7 @@ router.post("/create-checkout", async (req, res) => {
 
     const session = await stripe.checkout.sessions.create({
 
-      payment_method_types: ["paynow"],
+      payment_method_types: ["paynow", "card"],
 
       line_items: [
         {
